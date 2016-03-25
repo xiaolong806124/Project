3aca089cc2f7b8dde63d69fa95f14fe52499d374
zwl <xiaolong806124@126.com>
2016-03-25 19:25:40 +0800

    通过添加broadreceiver去监听网络的功能，修改了整个App与网络相关的部分，使整体功能显得更合理。现在问题是app与设备沟通良好时，不是网络断而是设备端断了，此时如果设备重启，app不能监听数据。该如何解决？ 做了一个小修改，即可以在在网络断开给出提示时，可以点击提示框来设置网络。

--- app/src/main/java/com/project/activities/ActivityMain.java
+++ app/src/main/java/com/project/activities/ActivityMain.java
@@ -1,13 +1,16 @@
 package com.project.activities;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -32,10 +35,26 @@ public class ActivityMain extends FragmentActivity {
         new Pager(ActivityMain.this).setPager();
 
         textView = (TextView) findViewById(R.id.id_txtInternet);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = null;
+                //that is android 3.0
+                if (android.os.Build.VERSION.SDK_INT > 10) {
+                    intent = new Intent(Settings.ACTION_SETTINGS);
+                } else {
+                    intent = new Intent();
+                    ComponentName component = new ComponentName("com.android.settings", "com.android.settings.Settings");
+                    intent.setComponent(component);
+                    intent.setAction("android.intent.action.VIEW");
+                }
+                ActivityMain.this.startActivity(intent);
+            }
+        });
         // 3. register a broadreceiver.
         registerMyReceiver();
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
